03f133b9f915d796dc2600cc13e3d0e471ae743f
youngalong <youngalone@163.com>
2023-11-29 23:07:40 +0800

    [fix] 修复参数不匹配bug

--- criteria/localitly_regularizer.py
+++ criteria/localitly_regularizer.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
-import wandb
 
+from config import config
 from config.config import regularizer_alpha, regularizer_l2_lambda, regularizer_lpips_lambda, latent_ball_num_of_samples
 from criteria import l2_loss
 
@@ -24,11 +24,11 @@ class SpaceRegularizer:
     def get_image_from_ws(self, w_codes, G):
         return torch.cat([G.synthesis(w_code, noise_mode='none', force_fp32=True) for w_code in w_codes])
 
-    def ball_holder_loss_lazy(self, new_G, num_of_sampled_latents, w_batch, log_name, use_wandb=False):
+    def ball_holder_loss_lazy(self, new_G, num_of_sampled_latents, w_batch, log_name):
         loss = 0.0
 
         z_samples = np.random.randn(num_of_sampled_latents, self.original_G.z_dim)
-        w_samples = self.original_G.mapping(torch.from_numpy(z_samples).to('cuda:0'), None,
+        w_samples = self.original_G.mapping(torch.from_numpy(z_samples).to(config.device), None,
                                             truncation_psi=0.5)
         territory_indicator_ws = [self.get_morphed_w_code(w_code.unsqueeze(0), w_batch) for w_code in w_samples]
 
@@ -39,17 +39,11 @@ class SpaceRegularizer:
 
             if regularizer_l2_lambda > 0:
                 l2_loss_val = l2_loss.l2_loss(old_img, new_img)
-                if use_wandb:
-                    wandb.log({f'losses/space_regularizer_l2_loss_val_{log_name}': l2_loss_val.detach().cpu()},
-                              commit=False)
                 loss += l2_loss_val * regularizer_l2_lambda
 
             if regularizer_lpips_lambda > 0:
                 loss_lpips = self.lpips_loss(old_img, new_img)
                 loss_lpips = torch.mean(torch.squeeze(loss_lpips))
-                if use_wandb:
-                    wandb.log({f'losses/space_regularizer_lpips_loss_val_{log_name}': loss_lpips.detach().cpu()},
-                              commit=False)
                 loss += loss_lpips * regularizer_lpips_lambda
 
         return loss / len(territory_indicator_ws)
